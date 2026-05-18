945c67bb90239799e43c0d2d611af75219a89e59
cuongnew87 <cuongnew37@gmail.com>
2026-05-18 18:47:00 +0700

    sửa thành hiển thị dưới dạng tab

--- src/app/pages/tao-file-yaml/tao-file-yaml.module.ts
+++ src/app/pages/tao-file-yaml/tao-file-yaml.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatRippleModule } from '@angular/material/core';
-import { NbButtonModule, NbCardModule, NbIconModule, NbInputModule, NbOptionModule, NbSelectModule, NbToggleModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbIconModule, NbInputModule, NbOptionModule, NbSelectModule, NbTabsetModule, NbToggleModule } from '@nebular/theme';
 import { TaoFileYamlComponent } from './tao-file-yaml.component';
 import { ModalComponent } from './modal/modal.component';
 import { CommonModule } from '@angular/common';
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
     NbOptionModule,
     NbToggleModule,
     NbIconModule,
+    NbTabsetModule,
     FormsModule
   ],
   declarations: [
